35e3326b07e1626858601e34a35d086ad0e50577
Alias M6 <aliasmontoya719@gmail.com>
2016-10-25 15:10:09 -0600

    added auth to the front-end

--- js/services.js
+++ js/services.js
@@ -4,24 +4,47 @@
 
   angular
     .module('myApp.services', [])
-    .service('coffeeService', coffeeService);
+    .service('coffeeService', coffeeService)
+    .service('userService', userService);
 
   coffeeService.$inject = ['$http'];
+  userService.$inject = ['$http'];
 
   function coffeeService($http) {
     /*jshint validthis: true */
-    const baseURL = 'https://secret-peak-70898.herokuapp.com/coffee';
+    const baseURL = 'https://secret-peak-70898.herokuapp.com/';
     this.getAllCoffee = function() {
       return $http.get(baseURL);
     };
     this.addCoffee = function(coffee) {
       return $http({
         method: 'POST',
-        url: baseURL,
+        url: baseURL + 'coffee',
         data: coffee,
         headers: {'Content-Type': 'application/json'}
       });
-    }
+    };
     this.greeting = 'hie';
+  }
+  function userService($http) {
+    /*jshint validthis: true */
+    const baseURL = 'https://secret-peak-70898.herokuapp.com/user/' ;
+    this.test = 'hi';
+    this.login = function(user) {
+      $http({
+        method: 'POST',
+        url: baseURL + 'login',
+        data: user,
+        headers: {'Content-Type': 'application/json'}
+      })
+    };
+    this.register = function(user) {
+      $http({
+        method: 'POST',
+        url: baseURL + 'register',
+        data: user,
+        headers: {'Content-Type': 'application/json'}
+      })
+    }
   }
 })();
